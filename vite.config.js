39ed2637e39ac6dd4f1e126fc49dd47491139e06
Eswaramoorthy karthikeyan <eswarkarthik95@gmail.com>
2025-06-25 23:38:15 +0530

    feat: add legacy browser support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: './',
-	plugins: [react()]
-});
+  base: './',
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+})
